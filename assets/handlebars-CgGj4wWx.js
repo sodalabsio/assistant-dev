d64608279c930371c3ce4fb36d8e71888ad6e97a
lachjames <lachlan_oneill@iinet.net.au>
2025-02-20 18:41:51 +1100

    Updates

--- assets/handlebars-CgGj4wWx.js
+++ assets/handlebars-CgGj4wWx.js
@@ -1,4 +1,4 @@
-import{m as i}from"./Assistant-Yg50h1nH.js";import"./index-BnT70kVk.js";/*!-----------------------------------------------------------------------------
+import{m as i}from"./Assistant-Cpslc0QE.js";import"./index-DaepbIkP.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
--- assets/javascript-B1Jg1ecS.js
+++ assets/javascript-B1Jg1ecS.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-bibM31bI.js";import"./Assistant-Yg50h1nH.js";import"./index-BnT70kVk.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-KI_pRtI4.js";import"./Assistant-Cpslc0QE.js";import"./index-DaepbIkP.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
