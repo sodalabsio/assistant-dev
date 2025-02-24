588ac524d04569b9b1ea115eb4566d1da7708cfd
lachjames <lachlan_oneill@iinet.net.au>
2025-02-24 16:22:49 +1100

    Updates

--- assets/javascript-DKSlSxvL.js
+++ assets/javascript-DKSlSxvL.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-B7I13U48.js";import"./index-CcS3RM2d.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-CpkWjK4-.js";import"./index-D9fKbr_l.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
--- assets/python-DAB3A03X.js
+++ assets/python-DAB3A03X.js
@@ -1,4 +1,4 @@
-import{co as o}from"./index-CcS3RM2d.js";/*!-----------------------------------------------------------------------------
+import{cg as o}from"./index-D9fKbr_l.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
