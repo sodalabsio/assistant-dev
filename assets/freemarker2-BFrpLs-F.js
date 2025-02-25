9ed02ddae2597ed5912b88faeead9cd2097f4651
lachjames <lachlan_oneill@iinet.net.au>
2025-02-25 12:58:37 +1100

    Updates

--- assets/freemarker2-BFrpLs-F.js
+++ assets/freemarker2-BFrpLs-F.js
@@ -1,4 +1,4 @@
-import{ch as f}from"./index-BIEp0EyG.js";/*!-----------------------------------------------------------------------------
+import{cl as f}from"./index-CHft_HdJ.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
