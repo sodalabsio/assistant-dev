2e4c9f83dba04078acecd6fff5f1fa4647e50c4f
lachjames <lachlan_oneill@iinet.net.au>
2025-03-03 18:17:34 +1100

    Updates

---
+++ assets/browser-BCQlru_x.js
@@ -0,0 +1,1 @@
+import{a0 as e}from"./index-B6o31y7z.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
