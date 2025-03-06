dc9e291fc4ffce639bdec22548af330037ede039
lachjames <lachlan_oneill@iinet.net.au>
2025-03-06 17:34:20 +1100

    Updates

--- assets/htmlMode-CAnoVF4H.js
+++ assets/htmlMode-CAnoVF4H.js
@@ -1,4 +1,4 @@
-import{m as lt}from"./Assistant-BgzOWdax.js";import"./index-VpeeDE1q.js";/*!-----------------------------------------------------------------------------
+import{m as lt}from"./Assistant-CVQj73y9.js";import"./index-Dm9ijs8p.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
--- assets/javascript-DUM92DaN.js
+++ assets/javascript-DUM92DaN.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-BMkAnKTp.js";import"./Assistant-BgzOWdax.js";import"./index-VpeeDE1q.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-D6222j1d.js";import"./Assistant-CVQj73y9.js";import"./index-Dm9ijs8p.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
