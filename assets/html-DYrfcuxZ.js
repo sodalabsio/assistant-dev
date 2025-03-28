c5e9a0e29f627c41d4a2b4e7ea3618b3b926b05e
lachjames <lachlan_oneill@iinet.net.au>
2025-03-28 11:17:03 +1100

    Updates

--- assets/html-DYrfcuxZ.js
+++ assets/html-DYrfcuxZ.js
@@ -1,4 +1,4 @@
-import{dz as s}from"./index-DtzRll9P.js";/*!-----------------------------------------------------------------------------
+import{dA as s}from"./index-BdOdv8ru.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
--- assets/python-DS06c-en.js
+++ assets/python-DS06c-en.js
@@ -1,4 +1,4 @@
-import{dz as o}from"./index-DtzRll9P.js";/*!-----------------------------------------------------------------------------
+import{dA as o}from"./index-BdOdv8ru.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
