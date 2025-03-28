a5464d9b22c819fc3ee5187da19d6f2701a75ec4
lachjames <lachlan_oneill@iinet.net.au>
2025-03-28 17:06:44 +1100

    Updates

--- assets/python-DWsTW00M.js
+++ assets/python-DWsTW00M.js
@@ -1,4 +1,4 @@
-import{dF as o}from"./index-C05eu76g.js";/*!-----------------------------------------------------------------------------
+import{dG as o}from"./index-BKxmIIo4.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
