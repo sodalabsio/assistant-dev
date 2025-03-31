a562c6027f55c8dbfc27c9f27b11287fc0dfccb8
lachjames <lachlan_oneill@iinet.net.au>
2025-03-31 21:53:46 +1100

    Updates

--- assets/python-wYXM-lrW.js
+++ assets/python-wYXM-lrW.js
@@ -1,4 +1,4 @@
-import{dL as o}from"./index-CeuP7Wcl.js";/*!-----------------------------------------------------------------------------
+import{dN as o}from"./index-Hd-7D8Fy.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
