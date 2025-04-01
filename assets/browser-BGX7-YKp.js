e1083f8c6ba97a2e7b5580af538965f631ae7e51
lachjames <lachlan_oneill@iinet.net.au>
2025-04-02 10:51:41 +1100

    Updates

--- assets/browser-BGX7-YKp.js
+++
@@ -1,1 +0,0 @@
-import{ay as e}from"./index-Dw1N3T8E.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
