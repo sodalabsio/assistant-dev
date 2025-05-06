a7bfcb8cc1f40b33b09271b98e6150594ce90c34
lachjames <lachlan_oneill@iinet.net.au>
2025-05-06 23:35:54 +1000

    Updates

--- assets/freemarker2-C3daAyZo.js
+++ assets/freemarker2-C3daAyZo.js
@@ -1,4 +1,4 @@
-import{ed as f}from"./index-Cv4c2dFn.js";/*!-----------------------------------------------------------------------------
+import{ed as f}from"./index-CkXxf3zP.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
--- assets/tsMode-DzCBhXAJ.js
+++ assets/tsMode-DzCBhXAJ.js
@@ -1,4 +1,4 @@
-import{ee as O,ed as I}from"./index-Cv4c2dFn.js";/*!-----------------------------------------------------------------------------
+import{ee as O,ed as I}from"./index-CkXxf3zP.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
