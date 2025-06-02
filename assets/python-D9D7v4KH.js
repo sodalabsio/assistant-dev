67af7ddda9a59abbb5790f8de9798c18250ecc6d
lachjames <lachlan_oneill@iinet.net.au>
2025-06-02 17:53:55 +1000

    Updates

--- assets/python-D9D7v4KH.js
+++ assets/python-D9D7v4KH.js
@@ -1,4 +1,4 @@
-import{ej as o}from"./index-dicrzqcl.js";/*!-----------------------------------------------------------------------------
+import{en as o}from"./index-BCd_pm6V.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
