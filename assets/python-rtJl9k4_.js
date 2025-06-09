9394e00898c13e6689e417c18e65131f4e540a32
lachjames <lachlan_oneill@iinet.net.au>
2025-06-09 18:02:48 +1000

    Updates

--- assets/python-rtJl9k4_.js
+++ assets/python-rtJl9k4_.js
@@ -1,4 +1,4 @@
-import{eY as o}from"./index-DEdewXZg.js";/*!-----------------------------------------------------------------------------
+import{fe as o}from"./index-CUl1D3_f.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
