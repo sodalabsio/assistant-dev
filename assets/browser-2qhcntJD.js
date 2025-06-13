29ccbb2d2e35137d8a6e9b9193cd4d952fe12308
lachjames <lachlan_oneill@iinet.net.au>
2025-06-14 02:16:25 +1000

    Updates

--- assets/browser-2qhcntJD.js
+++
@@ -1,1 +0,0 @@
-import{bh as o}from"./index-D_g5YZ5e.js";var r,e;function t(){return e||(e=1,r=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}),r}var s=t();const n=o(s),b=Object.freeze(Object.defineProperty({__proto__:null,default:n},Symbol.toStringTag,{value:"Module"}));export{b};
--- assets/mdx-BU5NFEIZ.js
+++ assets/mdx-BU5NFEIZ.js
@@ -1,4 +1,4 @@
-import{fF as s}from"./index-D_g5YZ5e.js";/*!-----------------------------------------------------------------------------
+import{fF as s}from"./index-CKZsPfL7.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
