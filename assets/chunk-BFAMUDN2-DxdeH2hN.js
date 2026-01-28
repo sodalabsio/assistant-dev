383049e855d45efc0e6db79b5284e0f1fd43b368
lachjames <lachlan_oneill@iinet.net.au>
2026-01-29 10:53:40 +1100

    Updates

--- assets/chunk-BFAMUDN2-DxdeH2hN.js
+++
@@ -1,1 +0,0 @@
-import{_ as s}from"./mermaid.core-Bo-8dFCy.js";import{s as o}from"./AssistantApp-o16BMeEj.js";var d=s((t,e)=>{let n;return e==="sandbox"&&(n=o("#i"+t)),(e==="sandbox"?o(n.nodes()[0].contentDocument.body):o("body")).select(`[id="${t}"]`)},"getDiagramElement");export{d as g};
