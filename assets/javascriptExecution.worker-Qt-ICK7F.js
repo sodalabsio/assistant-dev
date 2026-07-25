3e39792d512abe85e51df96e7eef463ae9b7b2a1
Lachlan <me@lachlanoneill.io>
2026-07-26 09:49:01 +1000

    Updates

--- assets/javascriptExecution.worker-Qt-ICK7F.js
+++
@@ -1,1 +0,0 @@
-function t(e){return new Worker("https://dev.assistant.sodalabs.io/assets/javascriptExecution.worker-CcgoTotl.js",{type:"module",name:e?.name})}export{t as default};
---
+++ assets/javascriptExecution.worker-iH0DOuBt.js
@@ -0,0 +1,1 @@
+function r(e){return new Worker("https://dev.assistant.sodalabs.io/assets/javascriptExecution.worker-Cr7ceZ5n.js",{type:"module",name:e?.name})}export{r as default};
