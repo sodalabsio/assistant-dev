f0cc7989c68403739a5caaecc8bd4fe9e525f9e8
Lachlan <me@lachlanoneill.io>
2026-08-01 09:46:53 +1000

    Updates

---
+++ assets/localSearchWorker-DhJptWNE.js
@@ -0,0 +1,1 @@
+function a(e){return new Worker("https://dev.assistant.sodalabs.io/assets/localSearchWorker-D_VeQD0s.js",{type:"module",name:e?.name})}export{a as default};
--- assets/localSearchWorker-Doxgry_v.js
+++
@@ -1,1 +0,0 @@
-function r(e){return new Worker("https://dev.assistant.sodalabs.io/assets/localSearchWorker-D1WPr4oj.js",{type:"module",name:e?.name})}export{r as default};
