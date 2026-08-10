44707de4a3d159d44eb15075e7ec9639c0f2724e
Lachlan <me@lachlanoneill.io>
2026-08-10 18:33:42 +1000

    Updates

--- assets/sqlite.worker-CwG59adn.js
+++
@@ -1,1 +0,0 @@
-function s(e){return new Worker("https://dev.assistant.sodalabs.io/assets/sqlite.worker-BcQGRj86.js",{type:"module",name:e?.name})}export{s as default};
---
+++ assets/sqlite.worker-CwabwxRQ.js
@@ -0,0 +1,1 @@
+function s(e){return new Worker("https://dev.assistant.sodalabs.io/assets/sqlite.worker-CLZTJG_b.js",{type:"module",name:e?.name})}export{s as default};
